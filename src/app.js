a13a056dabe41a4e0604fbe8ae82d7e682421fec
Noé Ibáñez Torres <132618491+Noesersi@users.noreply.github.com>
2023-07-11 21:56:18 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -4,6 +4,51 @@ import "./style.css";
 
 window.onload = () => {
   //write your code here
-  document;
+  document.querySelector("#btn").addEventListener("click", () => {
+    document.querySelector("#excusa").innerHTML = generateExcuse();
+  });
   console.log("Hello Rigo from the console!");
+};
+
+let generateExcuse = () => {
+  let sujeto = [
+    "El gato",
+    "El vecino de arriba",
+    "Mi tia la coja",
+    "La tortuga"
+  ];
+  let accion = [
+    "se ha comido",
+    "ha escupido en",
+    "me ha robado",
+    "ha explotado encima de"
+  ];
+  let posession = [
+    "mis llaves",
+    "mi móvil",
+    "mis deberes",
+    "mi coche",
+    "mi ordenador"
+  ];
+  let donde = [
+    "en la calle",
+    "en el parque",
+    "en el consejo de ministros",
+    "en la farmacia"
+  ];
+
+  let sujetoIndex = Math.floor(Math.random() * sujeto.length);
+  let accionIndex = Math.floor(Math.random() * accion.length);
+  let posessionIndex = Math.floor(Math.random() * posession.length);
+  let dondeIndex = Math.floor(Math.random() * donde.length);
+
+  return (
+    sujeto[sujetoIndex] +
+    " " +
+    accion[accionIndex] +
+    " " +
+    posession[posessionIndex] +
+    " " +
+    donde[dondeIndex]
+  );
 };
