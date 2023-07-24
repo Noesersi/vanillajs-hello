44f998aff916618fc97143ba79115893966c879d
Noé Ibáñez Torres <132618491+Noesersi@users.noreply.github.com>
2023-07-24 13:15:53 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -2,10 +2,54 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+let pronombres = ["el", "un"];
+let sustantivos = [
+  "perro",
+  "gato",
+  "cactus",
+  "tronco",
+  "alcantarilla",
+  "anillo"
+];
+let adjetivos = ["loco", "grasiento", "maloliente"];
+let extensiones = [".com", ".es", ".net"];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+const generadorDeDominios = () => {
+  let nombresDeDominio = [];
+
+  for (let i = 0; i < pronombres.length; i++) {
+    for (let j = 0; j < adjetivos.length; j++) {
+      for (let k = 0; k < sustantivos.length; k++) {
+        for (let l = 0; l < extensiones.length; l++) {
+          let nombreDeDominio =
+            pronombres[i] + adjetivos[j] + sustantivos[k] + extensiones[l];
+          nombresDeDominio.push(nombreDeDominio);
+        }
+      }
+    }
+  }
+
+  return nombresDeDominio;
+};
+
+const mostrarNombresDeDominio = () => {
+  const todosLosNombresDeDominio = generadorDeDominios();
+  let listaDeDominios = todosLosNombresDeDominio.join("<br>");
+  document.getElementById("generadorDeDominios").innerHTML = listaDeDominios;
+};
+
+window.onload = () => {
+  let generarBoton = document.getElementById("generar");
+  generarBoton.addEventListener("click", mostrarNombresDeDominio);
+
+  let eliminarBoton = document.getElementById("eliminar");
+  eliminarBoton.addEventListener(
+    "click",
+    () => {
+      document.getElementById("generadorDeDominios").innerHTML = "";
+    },
+    false
+  );
+
+  mostrarNombresDeDominio();
 };
